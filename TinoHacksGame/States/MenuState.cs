051be401ef68bc0faa1f45ca2f7986a9d3f6a568
TheBestestGuestest <gamegeek99@gmail.com>
2018-04-21 10:20:33 -0700

    working on the menu

--- TinoHacksGame/States/MenuState.cs
+++ TinoHacksGame/States/MenuState.cs
@@ -3,6 +3,10 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
 
 namespace TinoHacksGame.States
 {
@@ -17,6 +21,24 @@ namespace TinoHacksGame.States
         public MenuState()
         {
 
+        }
+        public override void Initialize(ContentManager Content) {
+            base.Initialize(Content);
+            
+        }
+
+        public override void Update(GameTime gameTime) {
+            base.Update(gameTime);
+            GamePadCapabilities capabilities = GamePad.GetCapabilities(PlayerIndex.One);
+            if (capabilities.IsConnected) {
+                GamePadState state = GamePad.GetState(PlayerIndex.One);
+                if (state.ThumbSticks.Left.X < -0.5f) Console.WriteLine("yeet");
+                if (state.ThumbSticks.Left.X > 0.5f) Console.WriteLine("yote");
+            }
+        }
+
+        public override void Draw(SpriteBatch spriteBatch, GraphicsDevice device) {
+            base.Draw(spriteBatch, device);
         }
     }
 }
