8da2934b24a569c0c8ff6d4afdf00fd78227abcc
Gertyerteg <Spencerchang2001@gmail.com>
2018-04-21 09:06:50 -0700

    Initial Commit

---
+++ TinoHacksGame/GameManager.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TinoHacksGame
+{
+    /// <summary>
+    /// Single-Ton object that manages different states of the game. Each state is
+    /// stored into a different class and is individually updated and drawn to the screen.
+    /// </summary>
+    public class GameManager
+    {
+        private static GameManager inst;
+        /// <summary>
+        /// Retrieves the single ton instance of the <c>GameManager</c>.
+        /// </summary>
+        /// <returns></returns>
+        public static GameManager GetInstance()
+        {
+            if (inst == null)
+                inst = new GameManager();
+
+            return inst;
+        }
+
+        /// <summary>
+        /// Creates a new instance of the <c>GameManager</c>.
+        /// </summary>
+        private GameManager()
+        {
+
+        }
+
+        /// <summary>
+        /// Updates the <c>GameManager</c>'s logic and conditional checking.
+        /// </summary>
+        /// <param name="gameTime">Provides a snapshot of timing values.</param>
+        public void Update(GameTime gameTime)
+        {
+
+        }
+
+        /// <summary>
+        /// Draws the <c>GameManager</c>'s current state to the screen.
+        /// </summary>
+        /// <param name="spriteBatch"></param>
+        public void Draw(SpriteBatch spriteBatch)
+        {
+
+        }
+    }
+}
---
+++ TinoHacksGame/State.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TinoHacksGame
+{
+    /// <summary>
+    /// A game state that's updated and drawn within the <c>GameManager</c>.
+    /// </summary>
+    public class State
+    {
+        /// <summary>
+        /// Creates a new instance of a <c>State</c>.
+        /// </summary>
+        public State()
+        {
+
+        }
+
+        /// <summary>
+        /// Updates the <c>State</c>'s conditional checking and logic.
+        /// </summary>
+        /// <param name="gameTime"></param>
+        public virtual void Update(GameTime gameTime)
+        {
+
+        }
+
+        /// <summary>
+        /// Draws the <c>State</c>.
+        /// </summary>
+        /// <param name="spriteBatch"></param>
+        /// <param name="device"></param>
+        public virtual void Draw(SpriteBatch spriteBatch, GraphicsDevice device)
+        {
+
+        }
+    }
+}
